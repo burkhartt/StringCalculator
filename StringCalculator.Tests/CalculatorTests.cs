3f90e05799e8066cc7b8df5e3b6969cdcb9115e2
burkhartt <timmyb_84@hotmail.com>
2013-06-13 15:19:13 -0400

    blah

---
+++ StringCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Machine.Specifications;
+using NUnit.Framework;
+
+namespace StringCalculator.Tests {
+    [TestFixture]
+    public class CalculatorTests {
+        [SetUp]
+        public void Setup() {
+            calculator = new Calculator();
+        }
+
+        private Calculator calculator;
+
+        [Test]
+        public void When_Five_numbers_are_passed_in_seperated_by_comma() {
+            calculator.Add("1,2,3,4,5").ShouldEqual(15);
+        }
+
+        [Test]
+        public void When_a_custom_delimiter_is_specified() {
+            calculator.Add("//;\n1;2").ShouldEqual(3);
+        }
+
+        [Test]
+        public void When_empty_string_return_zero() {
+            calculator.Add("").ShouldEqual(0);
+        }
+
+        [Test]
+        public void When_negative_numbers_are_passed_in_an_error_is_thrown() {
+            Assert.Throws<Exception>(() => calculator.Add("-1,2"), "Negative numbers are not allows. The negative numbers are -1");
+        }
+
+        [Test]
+        public void When_numbers_are_passed_in_seperated_by_comma_With_NewLine() {
+            calculator.Add("1\n2,3").ShouldEqual(6);
+        }
+
+        [Test]
+        public void When_one_number_is_greater_than_1000_should_be_ignored() {
+            calculator.Add("1001, 2").ShouldEqual(2);
+        }
+
+        [Test]
+        public void When_the_number_one_is_passed_in() {
+            calculator.Add("1").ShouldEqual(1);
+        }
+
+        [Test]
+        public void When_the_number_two_is_passed_in() {
+            calculator.Add("2").ShouldEqual(2);
+        }
+
+        [Test]
+        public void When_two_numbers_are_passed_in_seperated_by_comma() {
+            calculator.Add("2,2").ShouldEqual(4);
+        }
+
+        [Test]
+        public void Should_Support_Different_Delimiters_Length() {
+            calculator.Add("//[***]\n0***1***2").ShouldEqual(3);
+            //calculator.Add("//;\n1;2").ShouldEqual(3);
+        }
+    }
+}
---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculator {
+    public class Calculator {
+        string customDelimiter = "";
+         
+        public int Add(string numbers) {
+             if (string.IsNullOrEmpty(numbers)) {
+                 return 0;
+             }
+             
+             var startingPoint = 0;
+             customDelimiter = SetCustomDelimiter(numbers, ref startingPoint);
+
+             List<int> negativeNumbers;
+             var sum = DoSum(numbers, startingPoint, customDelimiter, out negativeNumbers);
+             if (negativeNumbers.Count > 0) {
+                 throw new Exception(string.Format("Negative numbers are not allows. The negative numbers are {0}", GetStringList(negativeNumbers)));
+             }
+
+             return sum;
+         }
+
+        private static int DoSum(string numbers, int startingPoint, string customDelimiter, out List<int> negativeNumbers) {
+            var sum = 0;
+            negativeNumbers = new List<int>();
+            foreach (var numberString in numbers.Substring(startingPoint).Split(new[] {",", "\n", customDelimiter }, StringSplitOptions.None)) {
+                if(string.IsNullOrEmpty(numberString)){continue;}
+                var number = int.Parse(numberString);
+                if (number < 0) {
+                    negativeNumbers.Add(number);
+                } else if (number > 1000) {
+                    continue;
+                } 
+                sum += number;
+            }
+            return sum;
+        }
+
+        private static string SetCustomDelimiter(string numbers, ref int startingPoint) {
+            string delimiter = string.Empty;
+            if (!numbers.StartsWith("//")) {
+                return delimiter;
+            }
+
+            if (numbers.StartsWith("//[")) {
+                var startIndexDelimiter = numbers.IndexOf("//[") + 3;
+                var endIndexDelimiter = numbers.IndexOf("]");
+                delimiter = numbers.Substring(startIndexDelimiter, endIndexDelimiter - startIndexDelimiter);
+                var indexOfNewLine = numbers.IndexOf("\n");
+                startingPoint = indexOfNewLine;
+                return delimiter;
+            }
+            if (numbers.StartsWith("//")) {
+                var indexOfNewLine = numbers.IndexOf("\n");
+                delimiter = numbers.Substring(2, indexOfNewLine - 2);
+                startingPoint = indexOfNewLine + delimiter.Length;
+            }
+            return delimiter;
+        }
+
+        private string GetStringList(IEnumerable<int> negativeNumbers) {
+            var stringList = negativeNumbers.Aggregate("", (current, negativeNumber) => current + (negativeNumber + ","));
+            return stringList.TrimEnd(',');
+        }
+    }
+}
